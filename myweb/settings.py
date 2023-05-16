5828db7fbee3e2ba227811790f074b464a9f188b
hwc <qwe@qq.com>
2023-05-16 09:12:27 +0800

    3

--- myweb/settings.py
+++ myweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2(qt$z(o2!iiq%1=auxfqu*j0nwbug!4$(g++y9ud3o8)qy!n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
